5969c8cb42ef4b5fd15a098188431e5aae234c63
saghar <behinaeen.saghar@gmail.com>
2022-05-30 19:59:08 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+
+import React, { useState } from 'react';
 import NavTabs from './components/NavTabs';
 import Home from './components/pages/Home';
 import Profile from './components/pages/Profile';
@@ -6,16 +7,19 @@ import Login from './components/pages/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signup from "./components/pages/Signup";
 const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return(
 
     <BrowserRouter>
-        <NavTabs />
+        <NavTabs isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
         <Routes>
+            
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
             <Route path="/signup" element={<Signup />} />
-
+            <Route path="*" component = {() => "404 NOT FOUND"}/>
         </Routes>
     </BrowserRouter>
     )

f4949b2995bb957d245dc98cebdb3e2eaaa26d40
Nicole Franklin <Ncfranklin14@gmail.com>
2022-06-06 21:15:04 -0700

    card images workingon homepage

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Card from '../card/Card';
-import {Carousel} from 'react-bootstrap';
-import {Container} from 'react-bootstrap';
-import {Col} from 'react-bootstrap';
-import {Row} from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
+// import {Container} from 'react-bootstrap';
+// import {Col} from 'react-bootstrap';
+// import {Row} from 'react-bootstrap';
 import './Home.css';
 
 export default function Home() {
@@ -24,60 +24,54 @@ export default function Home() {
       // console.log("/////////")
       console.log(types)
       // console.log(type)
-    }) 
+    })
   }, [])
   const getChallenge = (selectedType) => {
-   
+
     console.log(selectedType)
 
-    navigate(`/challenges/`, { state: { type: selectedType} });
-  
-  }
-  
-  return (
-    <div className ='homepage'>
-<Carousel className='container' variant="dark">
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="http://webvision.med.utah.edu/wp-content/uploads/2012/06/50-percent-gray.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption className='headers'>
-      <h3>Welcome to MoveMovement Mapper</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="http://webvision.med.utah.edu/wp-content/uploads/2012/06/50-percent-gray.jpg"
-      alt="Second slide"
-    />
-    <Carousel.Caption className='headers'>
-      <h3>Find Challenges to Join</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="http://webvision.med.utah.edu/wp-content/uploads/2012/06/50-percent-gray.jpg"
-      alt="Third slide"
-    />
+    navigate(`/challenges/`, { state: { type: selectedType } });
 
-    <Carousel.Caption className='headers'>
-      <h3>Create Challenges with your Friends!</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-<Container>
-<div className='homepage-cards'>
-<Row md={4}>
-    <Col className="card" key={{type}} style={{width: "18rem"}}>
-    {types.map((activity) => <Card  key={types.id} card={activity.challenge_type} getChallenge={getChallenge} setType={setType}/>)}
-  </Col>
-</Row>
-</div>
-</Container>
+  }
+
+  return (
+    <div className='homepage'>
+      <Carousel className='container' variant="dark">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://webvision.med.utah.edu/wp-content/uploads/2012/06/50-percent-gray.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption className='headers'>
+            <h3>Welcome to MoveMovement Mapper</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://webvision.med.utah.edu/wp-content/uploads/2012/06/50-percent-gray.jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption className='headers'>
+            <h3>Find Challenges to Join</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://webvision.med.utah.edu/wp-content/uploads/2012/06/50-percent-gray.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption className='headers'>
+            <h3>Create Challenges with your Friends!</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <div className='homepage-cards'>
+        {types.map((activity) => <Card key={types.id} card={activity.challenge_type} getChallenge={getChallenge} setType={setType} />)}
+      </div>
     </div>
-    );
+  );
 }

5ea98884ba06aa734f9d826af2c81e31236e0931
saghar <behinaeen.saghar@gmail.com>
2022-06-07 10:54:45 -0700

    5

--- src/components/pages/unitConvert/UnitConverter.js
+++ src/components/pages/unitConvert/UnitConverter.js
@@ -1,5 +1,6 @@
 
 import React, {  useState } from 'react';
+import './UnitConverter.css';
 const converter = require('length-distance-converter')
 
 export default function UnitConverter(props) {
@@ -32,10 +33,9 @@ export default function UnitConverter(props) {
     <>
 
       <h1>Unit Conventor</h1>
-      <form className="Form" onSubmit={handleFormSubmit}>
+      <form className="Form Conventor" onSubmit={handleFormSubmit}>
         <input placeholder="0" name="originalalue" type="number" value={originalValue} onChange={(e) => { setOriginalValue(e.target.value) }} />
-       {/* <label>{props.challenge.unit}</label> */}
-        <select name="unit" value={unit} onChange={(e) => { setUnit(e.target.value) }}>
+        <select className="unit" name="unit" value={unit} onChange={(e) => { setUnit(e.target.value) }}>
           <option value="mile">km to mile</option>
           <option value="km">mile to km</option>
           <option value="feet">meter to feet</option>

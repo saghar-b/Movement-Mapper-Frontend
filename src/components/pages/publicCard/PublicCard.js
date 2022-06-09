f17319d242b0ee58dbfe0fdb45fb5cb497ed6673
saghar <behinaeen.saghar@gmail.com>
2022-06-09 15:21:50 -0700

    patricipants

--- src/components/pages/publicCard/PublicCard.js
+++ src/components/pages/publicCard/PublicCard.js
@@ -2,18 +2,37 @@ import React, { useEffect, useState } from 'react';
 import Moment from 'moment';
 import jwt from 'jwt-decode'
 import './PublicCard.css';
-import {getBaseUrl} from '../../../utils/API';
+import { getBaseUrl } from '../../../utils/API';
 import "../../../global.css";
 import userImage from '../../../assets/user.png'
 
 function PublicCard({ challenge, getoneChallenge, token }) {
 
+    const [isPast, setIsPast] = useState(false);
+    const [participants, setParticipants] = useState([]);
     const [isJoined, setIsJoined] = useState(false);
     useEffect(() => {
+        fetch(`${getBaseUrl()}/challenge/${challenge.id}`, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json()).then(data => {
+
+            setParticipants(data.scores)
+        })
+
+        const today = new Date();
+        const start = new Date(challenge.start_time);
+        const end = new Date(challenge.end_time);
+        if (end < today ) {
+            setIsPast(true)
+        } else {
+            setIsPast(false)
+        }
 
         checkJoined();
     }, [])
-    
+
     console.log("challenge");
     console.log(challenge);
     function handleChallengeClick(e) {
@@ -23,26 +42,24 @@ function PublicCard({ challenge, getoneChallenge, token }) {
     }
     function handleJoinBtn(e) {
         e.preventDefault();
-       
+
         if (token != "") {
-            
+
             const addChallenge = {
                 challenge_id: challenge.id,
                 user_id: jwt(token).id,
                 distance: "0",
-                join :true
+                join: true
             }
-         
+
             insertToDB(addChallenge)
         } else {
             alert("please log in")
         }
     }
     function checkJoined() {
-    console.log("hahahaha")
-    console.log(token)
         if (token != "") {
-          
+
             // check the use is joined
             fetch(`${getBaseUrl()}/challenges/score/${jwt(token).id}/${challenge.id}`, {
                 headers: {
@@ -54,11 +71,11 @@ function PublicCard({ challenge, getoneChallenge, token }) {
                     setIsJoined(false)
                 } else {
                     setIsJoined(true)
-                    console.log("joined")
+
 
                 }
             })
-        } 
+        }
         else {
             console.log("Notloged in")
 
@@ -93,8 +110,6 @@ function PublicCard({ challenge, getoneChallenge, token }) {
             }
         });
         if (response.ok) {
-            console.log("response")
-            console.log(response)
             window.location.reload(false);
         } else {
             alert('Failed to delete');
@@ -108,28 +123,28 @@ function PublicCard({ challenge, getoneChallenge, token }) {
                     <div className='card-header1'>
                         <h1 className='public-title' data-type={challenge.id} onClick={handleChallengeClick}>{challenge.Challenge_name}</h1>
                         <div className='public-img cursor-hand'>
-                            <img data-type={challenge.id} onClick={handleChallengeClick} src={challenge.picture_path} alt="a challenge to overcome"/>
+                            <img data-type={challenge.id} onClick={handleChallengeClick} src={challenge.picture_path} alt="a challenge to overcome" />
                         </div>
-                   </div>
-                
+                    </div>
+
                     <div className='card-body1'>
                         <div className='participant-circle'>
-                            <div className='participant-number'>{challenge.scores.length}</div>    
+                            <div className='participant-number'>{participants.length}</div>
                         </div>
                         <img src={userImage} className='icon' alt='user'></img>
                         <div className="public-card-body">
-                        <h6>Created by</h6> 
+                            <h6>Created by</h6>
                             <h4>{challenge.creator.user_name}</h4>
                             <h4>{Moment(challenge.start_time).format('MMM DD yyyy')}</h4>
                             <h4>{Moment(challenge.end_time).format('MMM DD yyyy')}</h4>
-                        
-                            <div className="cardButtons">   
-                        {isJoined &&
-                            <button className="button" onClick={handleLeaveBtn}>Leave</button>
-                        }
-                        {!isJoined &&
-                            <button className="button" onClick={handleJoinBtn}>Join</button>
-                        }
+
+                            <div className="cardButtons">
+                                {(isJoined && !isPast) &&
+                                    <button className="button" onClick={handleLeaveBtn}>Leave</button>
+                                }
+                                {(!isJoined && !isPast) &&
+                                    <button className="button" onClick={handleJoinBtn}>Join</button>
+                                }
                             </div>
                         </div>
 
